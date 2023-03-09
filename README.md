dac862edebab50cbef7fc4269030626522fb5cb4
nachipsyc <scarl.natsu729@gmail.com>
2023-03-09 23:27:22 +0900

    readmeの記述

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# resize
-resize photo files
+# About
+
+Resize photo files in specified directory at once.
+
+# Use
+
+go run resize.go *1 *2 *3 *4
+
+*1…ファイル取得元ディレクトリの相対パス
+*2…書き出し先ディレクトリの相対パス
+*3…リサイズ対象とするファイルのファイルサイズ上限閾値
+（Byte 単位）
+*4…リサイズの際の一辺の変更倍率
+（省略可能、省略した場合は同じ長さで書き出しを行う）
--- resize.go
+++ resize.go
@@ -10,36 +10,48 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strconv"
 
 	"github.com/nfnt/resize"
 )
 
 var source_dir string = ""
-var resize_option = ""
+var target_dir string = ""
+var upper_limit int64
+var magnification float64
 
 func main() {
 	// 入力をパース
 	flag.Parse()
 
-	// 対象ディレクトリを引数から取得(string)
+	// 読み込み対象ディレクトリのパスを引数から取得(string)
 	source_dir = flag.Arg(0)
 
+	// 書き出し対象ディレクトリのパスを引数から取得(string)
+	target_dir = flag.Arg(1)
+
+	// 画像のファイルサイズの上限を引数から取得(int)
+	upper_limit, _ = strconv.ParseInt(flag.Arg(2),10, 64)
+
 	// リサイズのオプションが指定された場合はセット
-	resize_option = flag.Arg(1)
+	magnification, _ = strconv.ParseFloat(flag.Arg(3), 64)
 
 	// 対象ディレクトリの中の全ファイルを取得([]os.FileInfo)
 	files, err := getFiles(source_dir)
 
+	// エラーがある場合は出力
 	if err != nil {
 		log.Fatal(err)
 		panic(err)
 	}
 
 	// 取得したファイルからJPEGファイルのみを抽出([]os.FileInfo)
-	jpeg_files := extractJpegImages(files)
+	jpeg_files := extractJpegFiles(files)
 
-	// サイズが20MB以上なら20MB未満になるまで解像度を落とす
+	// 画像を書き出し(サイズが指定した値以上のものはリサイズ処理を行う)
 	resizeJpegFiles(jpeg_files)
+
+	// encodeImages()として割り出し
 }
 
 // used by main()
@@ -56,7 +68,7 @@ func getFiles(source_dir string) ([]os.FileInfo, error) {
 }
 
 // used by main()
-func extractJpegImages(files []os.FileInfo) []os.FileInfo {
+func extractJpegFiles(files []os.FileInfo) []os.FileInfo {
 	var jpeg_images []os.FileInfo
 	for _, file := range files {
 		switch filepath.Ext(file.Name()) {
@@ -69,45 +81,7 @@ func extractJpegImages(files []os.FileInfo) []os.FileInfo {
 	return jpeg_images
 }
 
-// used by decodeImage()
-func conversionToReader(jpeg_file os.FileInfo) (io.Reader, error) {
-	io_file, err := os.Open(source_dir + "/" + jpeg_file.Name())
-	if err != nil {
-		return nil, err
-	}
-	return io_file, nil
-}
 
-// used by resizeJpegFiles()
-func decodeImage(jpeg_file os.FileInfo) (image.Image, error) {
-	io_file, err := conversionToReader(jpeg_file)
-	if err != nil {
-		return nil, err
-	}
-
-	decoded_image, _, err := image.Decode(io_file)
-	if err != nil {
-		return nil, err
-	}
-	return decoded_image, nil
-}
-
-// used by resizeJpegFiles()
-func encodeImage(resized_image image.Image, file_name string) error {
-	output, err := os.Create(source_dir + "/" + file_name)
-	if err != nil {
-		return err
-	}
-
-	defer output.Close()
-
-	opts := &jpeg.Options{Quality: 100}
-	if err := jpeg.Encode(output, resized_image, opts); err != nil {
-		return err
-	}
-
-	return nil
-}
 
 // used by main()
 func resizeJpegFiles(jpeg_files []os.FileInfo) {
@@ -117,24 +91,67 @@ func resizeJpegFiles(jpeg_files []os.FileInfo) {
 		decoded_image, _ := decodeImage(jpeg_file)
 		// 画像の横幅を取得
 		image_width := float64(decoded_image.Bounds().Dx())
-
-		// リサイズのオプションが指定されていたら幅を0.9倍
-		if resize_option != "" {
-			image_width *= 0.9
+		
+		// リサイズの倍率が指定されていたら幅を変更
+		if magnification != 0 {
+			image_width *= float64(magnification)
 		}
-
+		
 		var resized_image image.Image
-
-		if jpeg_file.Size() >= 20000000 {
+		
+		if jpeg_file.Size() >= upper_limit {
 			// 画像のリサイズ
 			resized_image = resize.Resize(uint(image_width), 0, decoded_image, resize.Lanczos3)
-
-			if resized_image != nil {
-				// リサイズした画像を書き込み
-				encodeImage(resized_image, jpeg_file.Name())
+		}
+		
+		if resized_image != nil {
+			// リサイズした画像を書き込み
+			encodeImage(resized_image, jpeg_file.Name())
+			} else if (decoded_image != nil) {
+				// デコードした画像を書き込み
+				encodeImage(decoded_image, jpeg_file.Name())
 			}
 		}
-	}
 
-	fmt.Println("resize completed!!")
-}
+		fmt.Println("resize completed!!")
+	}
+	
+	// used by resizeJpegFiles()
+	func decodeImage(jpeg_file os.FileInfo) (image.Image, error) {
+		io_file, err := conversionToReader(jpeg_file)
+		if err != nil {
+			return nil, err
+		}
+		
+		decoded_image, _, err := image.Decode(io_file)
+		if err != nil {
+			return nil, err
+		}
+		return decoded_image, nil
+	}
+	
+	// used by resizeJpegFiles()
+	func encodeImage(resized_image image.Image, file_name string) error {
+		output, err := os.Create(target_dir + "/" + file_name)
+		if err != nil {
+			return err
+		}
+		
+		defer output.Close()
+		
+		opts := &jpeg.Options{Quality: 100}
+		if err := jpeg.Encode(output, resized_image, opts); err != nil {
+			return err
+		}
+		
+		return nil
+	}
+	
+	// used by decodeImage()
+	func conversionToReader(jpeg_file os.FileInfo) (io.Reader, error) {
+		io_file, err := os.Open(source_dir + "/" + jpeg_file.Name())
+		if err != nil {
+			return nil, err
+		}
+		return io_file, nil
+	}
